6624fd79fa1a48bbe60fd016333fc87d1fe559b1
sena@s73.work <Sergey>
2024-03-17 13:54:47 +0100

    доксидока для netapp

--- netapp.h
+++ netapp.h
@@ -3,9 +3,16 @@
 #include <utility>
 #include <boost/asio.hpp>
 
+
+/** @brief приложение TCP сервер */
 class NetApp
 {
 public:
+    /**
+     * @brief конструктор
+     * @param io_context контекст ввода-вывода бустасио
+     * @param port номер TCP порта на котором сервер будет принимать соединения
+     * @param N статический размер блока команд */
   NetApp(boost::asio::io_context& io_context, unsigned short port, unsigned N);
 
 private:
